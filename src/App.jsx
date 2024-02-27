b7e4ae44adaf703e09aef64fb0139108984bf405
Gitam <gitam.shimoni@gmail.com>
2024-02-27 15:50:15 +0200

    Change background, add github button on top left

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import Homepage from "./Components/Homepage";
 import "./App.css";
+import Github from "./Components/Github";
 
 function App() {
   return (
     <div className="appmain-container">
+      <Github />
       <Homepage />
     </div>
   );
---
+++ src/Components/Github.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Github.css";
+
+const Github = () => {
+  return (
+    <div id="github-repo-button">
+      <a
+        id="github-repo-text"
+        href="https://github.com/GitamShimoni/Gitams_Projects"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Click for Github Repository
+      </a>
+    </div>
+  );
+};
+
+export default Github;
